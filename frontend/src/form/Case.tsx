4024cbd2afcaa2384a16dfe9ab89421592b41cc6
Jay Bhagat <user.email>
2019-07-05 13:26:36 -0400

    date functionality added and enum added for case comp

--- frontend/src/form/Case.tsx
+++ frontend/src/form/Case.tsx
@@ -6,16 +6,32 @@ import Checkbox from '@material-ui/core/Checkbox';
 import RadioButton from './tools/RadioButton'
 import DropDown from './tools/DropDown'
 import UploadForm from './UploadForm'
-import React from 'react';
+import React, { useEffect } from 'react';
 
 export interface HeaderProps {
     title: string
 }
 
+//migrate this to another file
+const DateField = () => {
+    return (
+    <TextField
+        required
+        id={"date"}
+        defaultValue={"12/06/2018"}
+        fullWidth
+        type={"date"}
+        />
+    )
+}
+
 export const RenderOptions = {
     TextField,
-    DropDown
+    DropDown,
+    DateField
 }
+
+type RenderComponent = keyof typeof RenderOptions
 
 export interface GridProps {
     xs: number,
@@ -27,6 +43,8 @@ export interface GridProps {
     autoComplete: string,
     variant?: string,
     dropDownOptions: (string | number)[] | boolean,
+    type?: string,
+    defaultValue: string | Date,
 }
 
 export const Header = (props: HeaderProps) => {
@@ -36,41 +54,43 @@ export const Header = (props: HeaderProps) => {
 }
 
 export const GridContainer = (props: GridProps) => {
-    const WhichComponentRenders = (props.componentName === 'TextField') ? RenderOptions['TextField'] : RenderOptions['DropDown']
+    const isDate = (props.name === "date") ? "date" : "type"
+    const [render, setRender] = React.useState<RenderComponent>("TextField")
+    useEffect(() => {
+        if (props.componentName === 'TextField' && props.name === 'date') {
+            setRender("DateField")
+        } else if (props.componentName === 'DropDown') {
+            setRender("DropDown")
+        } else {
+            setRender("TextField")
+        }
+    }, [props.id])
+    const WhichComponentRenders = RenderOptions[render]
+    const {id, name, label, dropDownOptions, autoComplete} = props
     return (
         <Grid item xs={12} sm={6}>
-            {
-                (props.componentName === 'TextField') ? (
-                    <WhichComponentRenders 
-                        required 
-                        id={props.id} 
-                        name={props.name}
-                        label={props.label}
-                        fullWidth
-                        dropDownOptions={false}
-                        autoComplete={props.autoComplete}
-                    />
-                ) : (
-                    <WhichComponentRenders
-                        id={props.id}
-                        name={props.name}
-                        label={props.label}
-                        dropDownOptions={props.dropDownOptions}
-                        autoComplete={props.autoComplete}
-                     />
-                )
-            }
+            <WhichComponentRenders 
+                required 
+                id={id}
+                name={name}
+                label={label}
+                fullWidth
+                dropDownOptions={dropDownOptions}
+                autoComplete={autoComplete}
+                />
         </Grid>
     )
 }
 
 export const testNames = ['Jon Snow', 'Sansa Stark', `Jaqen H'ghar`, 'Missandei']
 
+export const testCourtRooms = [`King's County`, `Queen's Criminal Court`, `Midtown Community Court`]
+
 const fieldsToPopulate = [
     {name: "date", label: "Date", type:"TextField", dropDownOptions:false},
     {name: "volunteerName", label: "Volunteer Name", type:"DropDown", dropDownOptions:[...testNames, 'Zoe Adel']},
     {name: "borough", label: "Borough", type:"DropDown", dropDownOptions:['Brooklyn', 'Queens', 'Bronx', 'Manhattan', 'Staten Island']},
-    {name: "courtroom", label: "Courtroom", type:"DropDown", dropDownOptions:false},
+    {name: "courtroom", label: "Courtroom", type:"DropDown", dropDownOptions:[...testCourtRooms, `210 Joralemon Street`]},
     {name: "judge", label: "Judge", type:"TextField", dropDownOptions:false},
     {name: "ada", label: "ADA", type:"TextField", dropDownOptions:false},
     {name: "docket", label: "Docket", type:"TextField", dropDownOptions:false},
@@ -79,6 +99,14 @@ const fieldsToPopulate = [
 
 const Case = () => {
     const duration = ['< 2 min', '2-5 min', '5+ min']
+    const today = () => {
+        let returnDate: string | Date = new Date()
+        let dd = String(returnDate.getDate()).padStart(2, '0')
+        let mm = String(returnDate.getMonth()+1).padStart(2, '0')
+        let yyyy = returnDate.getFullYear()
+        returnDate = mm + "/" + dd + "/" + yyyy
+        return returnDate
+    }
   return (
     <div>
         <UploadForm/>
@@ -86,6 +114,7 @@ const Case = () => {
         <Grid container spacing={2}>
           {
               fieldsToPopulate.map(val => {
+                  const dateDefault = (val.name === 'date') ? today() : ""
                   return <GridContainer 
                     xs={12}
                     componentName={val.type}
@@ -94,6 +123,7 @@ const Case = () => {
                     label={val.label}
                     autoComplete={val.name}
                     dropDownOptions={val.dropDownOptions}
+                    defaultValue={dateDefault}
                 />
               })
           }
